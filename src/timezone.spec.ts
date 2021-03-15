50ed5a0d13b8bd15c43dfb1fb43fd26b9d44bfde
Kerry Shetline <kerry@shetline.com>
2021-03-15 14:36:57 -0400

    Add negative length option to toIsoString(). Add Timezone.version. Trim spurious beginning transition entry. Update to use timezone data generated by @tubular/time-tzdb. (#7)

--- src/timezone.spec.ts
+++ src/timezone.spec.ts
@@ -2,6 +2,8 @@ import { expect } from 'chai';
 import { Timezone } from './timezone';
 import timezoneLarge from './timezone-large';
 
+const version = timezoneLarge.version;
+
 describe('Timezone', () => {
   beforeEach(() => Timezone.defineTimezones(Object.assign({
     'America/Barberg': 'America/Chicago',
@@ -10,6 +12,7 @@ describe('Timezone', () => {
 
   it('should properly create Timezone instances', () => {
     expect(Timezone.from('America/New_York').aliasFor).to.equal(null);
+    expect(Timezone.from('Antarctica/Troll').aliasFor).to.equal(null);
     expect(Timezone.from('America/New_York').population).to.be.greaterThan(20000000);
     expect(Timezone.from('America/New_York').supportsCountry('US')).to.be.true;
     expect(Timezone.from('America/New_York').supportsCountry('AD')).to.be.false;
@@ -21,12 +24,13 @@ describe('Timezone', () => {
 
   it('should properly guess matching timezones', () => {
     expect(Timezone.guess(true, 'US', 'US/Eastern')).to.equal('America/New_York');
-    expect(Timezone.guess(true, 'CA', 'US/Eastern')).to.equal('America/Montreal');
+    expect(Timezone.guess(true, 'CA', 'US/Eastern')).to.equal('America/Toronto');
     expect(Timezone.guess(true, 'LY', 'Africa/Cairo')).to.equal('Africa/Tripoli');
     Timezone.guess(true);
   });
 
   it('should properly perform Timezone utility functions', () => {
+    expect(Timezone.version).to.equal(version);
     expect(Timezone.from('America/Fooberg').population).to.equal(250);
     expect(Timezone.getPopulation('America/Fooberg')).to.equal(250);
     expect(Timezone.from('America/Fooberg').supportsCountry('xx')).to.be.true;
