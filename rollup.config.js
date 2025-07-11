620c27031704f1613cba95fc7c6a59af1f181011
Kerry Shetline <kerry@shetline.com>
2025-07-10 20:49:11 -0400

    Fix UMD packaging. (#52)

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,52 @@
+const nodeResolve = require('@rollup/plugin-node-resolve');
 const sourcemaps = require('rollup-plugin-sourcemaps');
 const terser = require('@rollup/plugin-terser');
 const typescript = require('@rollup/plugin-typescript');
 const pkg = require('./package.json');
 
-// noinspection JSUnusedGlobalSymbols
-module.exports = [{
-  external: ['by-request', 'json-z', '@tubular/math', '@tubular/util'],
-  input: 'src/index.ts',
-  output: [
-    {
+function trimUmd() {
+  // noinspection JSUnusedGlobalSymbols
+  return {
+    name: 'mixExports',
+    renderChunk(code, _chunk, options) {
+      if (options.format === 'umd') {
+        // Strip out dynamic import() so it doesn't generate warnings.
+        code = code.replace(/return import\(.*?\/\* webpackIgnore: true \*\/.*?tseuqer-yb.*?\.join\(''\)\)/s, 'return null');
+        // Strip out large and large-alt timezone definitions from UMD build.
+        code = code.replace(/(?<!timezoneSmall = )\/\* trim-file-start \*\/.*?\/\* trim-file-end \*\//sg, 'null');
+      }
+
+      return { code, map: null };
+    }
+  };
+}
+
+const plugins = [
+  nodeResolve(),
+  typescript({ inlineSources: true }),
+  trimUmd(),
+  sourcemaps(),
+  terser({
+    output: {
+      comments: (node, comment) => {
+        return comment.type === 'comment2' && /\b(webpackIgnore|vite-ignore)\b/.test(comment.value);
+      },
+      max_line_len: 511
+    },
+    sourceMap: { includeSources: true }
+  })
+];
+
+const onwarn = (code, defaultHandler) => {
+  if (code.code !== 'MIXED_EXPORTS')
+    defaultHandler(code);
+};
+
+// noinspection JSUnusedGlobalSymbols,CommaExpressionJS
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: {
       file: pkg.browser,
       sourcemap: true,
       format: 'umd',
@@ -20,32 +58,25 @@ module.exports = [{
         'by-request': '_by_request_' // Never used, only specified to suppress warning
       }
     },
-    {
-      file: pkg.main,
-      sourcemap: true,
-      format: 'cjs'
-    },
-    {
-      file: pkg.module,
-      sourcemap: true,
-      format: 'esm'
-    }
-  ],
-  plugins: [
-    typescript({ inlineSources: true }),
-    sourcemaps(),
-    terser({
-      output: {
-        comments: (node, comment) => {
-          return comment.type === 'comment2' && /\b(webpackIgnore|vite-ignore)\b/.test(comment.value);
-        },
-        max_line_len: 511
+    plugins,
+    onwarn
+  },
+  {
+    external: ['by-request', 'json-z', '@tubular/math', '@tubular/util'],
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        sourcemap: true,
+        format: 'cjs'
       },
-      sourceMap: { includeSources: true }
-    })
-  ],
-  onwarn(code, defaultHandler) {
-    if (code.code !== 'MIXED_EXPORTS')
-      defaultHandler(code);
+      {
+        file: pkg.module,
+        sourcemap: true,
+        format: 'esm'
+      }
+    ],
+    plugins: (plugins.slice(0).splice(0, 1).splice(2, 1), plugins),
+    onwarn
   }
-}];
+];
